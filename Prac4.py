fc2e6ea509299ee88d63f455c7f1b0c1619f8258
bhargavbakrania03 <97829224+bhargavbakrania03@users.noreply.github.com>
2022-01-29 00:19:48 +0530

    Update Prac4.py

--- Prac4.py
+++ Prac4.py
@@ -8,11 +8,15 @@ GITHUB REPOSITORY LINK : https://github.com/bhargavbakrania03/python_programs/ed
 
 tot_runners = int(input())
 runners = list(map(int, input().split(' ')))
+error = 0
 
-runners.sort()  # sorting the list for our ease
-runners.reverse()  # reversing the list to find the desired element quickly
-
-for i in runners:
-    if i < max(runners):  # finding the first smaller element than maximum element in the list
-        print(i)
-        break  # terminating the loop after finding desired element
+if len(runners) > tot_runners:  # to check that entered size and number of contestants matches or not
+    print('Please enter scores according to the number of contestants !')
+    error = 1
+else:
+    runners.sort()  # sorting the list for our ease
+    runners.reverse()  # reversing the list to find the desired element quickly
+    for i in runners:
+        if i < max(runners):  # finding the first smaller element than maximum element in the list
+            print(i)
+            break  # terminating the loop after finding desired element

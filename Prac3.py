c66ac152da5bf9a5d889abb3d22243bc6779f740
bhargavbakrania03 <97829224+bhargavbakrania03@users.noreply.github.com>
2022-01-29 00:01:56 +0530

    Update Prac3.py

--- Prac3.py
+++ Prac3.py
@@ -1,17 +1,26 @@
 """
 ID : 20CE001
 NAME : BHARGAV BIPINBHAI BAKRANIA
-GITHUB REPOSITORY LINK : https://github.com/bhargavbakrania03/python_programs/blob/main/Prac3.py
+GITHUB REPOSITORY LINK : https://github.com/bhargavbakrania03/python_programs/edit/main/Prac3.py
 """
 
 # Find Captain Room Number
 
 rep_room = int(input())  # total number of repeated rooms
 room_no = list(map(int, input().split(' ')))  # input for room number
+captain = []
+error = 0
 
 for i in range(len(room_no)):
     temp = room_no.count(i)
     if temp == 1:
-        captain_room = i  # storing captain's room in its variable
+        c_room = i  # storing captain's room in its variable
+        captain.append(i)
+    elif temp != rep_room:
+        error = 1
 
-print(captain_room)
+if error == 1:
+    print('Please enter proper groups !')
+else:
+    print(c_room)
+

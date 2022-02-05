71e79a6820fc12d9f4f94dce308be3ece3e5fba0
bhargavbakrania03 <97829224+bhargavbakrania03@users.noreply.github.com>
2022-02-05 23:07:21 +0530

    Update Prac5.py

--- Prac5.py
+++ Prac5.py
@@ -8,11 +8,24 @@ GITHUB REPOSITORY LINK : https://github.com/bhargavbakrania03/python_programs/ed
 # all lowercase letters to uppercase letters and vice versa.
 
 input_str = input()
+output_str = '';
+error = 0
 
 for i in input_str:
     if i.islower():  # checking whether the selected character is in lowercase or not
-        print(i.upper(), end='')  # converting that character in uppercase
+        output_str += output_str.join(i.upper())  # converting that character in uppercase and storing it in a variable
     elif i.isupper():  # checking whether the selected character is in uppercase or not
-        print(i.lower(), end='')  # converting that character in lowercase
+        output_str += output_str.join(i.lower())  # converting that character in lowercase and storing it in a variable
+    elif i == ' ':
+        output_str += output_str.join(i)  # inserting other special characters as it is
+    elif i.isdigit():
+        print('Error! String expected, got a number except characters')
+        error = 1
+        break
     else:
-        print(i, end='')  # printing other special characters as it is
+        print('Special characters are not allowed !!')
+        error = 1
+        break
+
+if error != 1:
+    print(output_str)
